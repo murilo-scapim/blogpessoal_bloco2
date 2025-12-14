9b1fc172e7e97b26b09fe21aebe2dec084a80356
murilo-scapim <mhscapim@gmail.com>
2025-12-14 12:29:12 -0300

    feat(usuario): implementa demais métodos na camada de service

--- src/usuario/services/usuario.service.ts
+++ src/usuario/services/usuario.service.ts
@@ -20,11 +20,45 @@ export class UsuarioService {
     });
   }
 
+  async findAll(): Promise<Usuario[]> {
+    return await this.usuarioRepository.find({
+      relations: {
+        postagem: true,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!usuario)
+      throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+
+    return usuario;
+  }
+
   async create(usuario: Usuario): Promise<Usuario> {
     const buscaUsuario = await this.findByUsuario(usuario.usuario);
 
     if (buscaUsuario)
       throw new HttpException('O usuário já existe', HttpStatus.BAD_REQUEST);
+
+    usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
+    return await this.usuarioRepository.save(usuario);
+  }
+
+  async update(usuario: Usuario): Promise<Usuario> {
+    const buscaUsuario = await this.findByUsuario(usuario.usuario);
+
+    if (!buscaUsuario)
+      throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
+
+    if (buscaUsuario && buscaUsuario.id !== usuario.id)
+      throw new HttpException('Usuário já cadastrado!', HttpStatus.BAD_REQUEST);
 
     usuario.senha = await this.bcrypt.criptografarSenha(usuario.senha);
     return await this.usuarioRepository.save(usuario);
