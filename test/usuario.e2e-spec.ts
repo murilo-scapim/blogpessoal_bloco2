56c49b4434e1c8f0083160fbfd3f259a47307795
murilo-scapim <mhscapim@gmail.com>
2025-12-14 12:02:20 -0300

    test(e2e): adiciona testes e2e para os módulos de Usuario e Auth

--- test/usuario.e2e-spec.ts
+++ test/usuario.e2e-spec.ts
@@ -3,8 +3,12 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import request from 'supertest';
 
 describe('Teste dos módulos Usuario e Auth (e2e)', () => {
+  let token: string;
+  let usuarioId: number;
+
   let app: INestApplication<App>;
 
   beforeAll(async () => {
@@ -21,12 +25,77 @@ describe('Teste dos módulos Usuario e Auth (e2e)', () => {
       ],
     }).compile();
 
-    afterAll(async () => {
-      await app.close();
-    });
-
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe());
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('1 - Deve cadastrar um novo Usuario', async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/usuarios/cadastrar')
+      .send({
+        nome: 'Murilo',
+        usuario: 'murilo@email.com',
+        senha: 'murilo123',
+        foto: 'https://i.imgur.com/zEM4Z3S.jpeg',
+      })
+      .expect(201);
+
+    usuarioId = (resposta.body as { id: number }).id;
+  });
+
+  it('2 - Não deve cadastrar um Usuario duplicado', async () => {
+    await request(app.getHttpServer())
+      .post('/usuarios/cadastrar')
+      .send({
+        nome: 'Murilo',
+        usuario: 'murilo@email.com',
+        senha: 'murilo123',
+        foto: 'https://i.imgur.com/zEM4Z3S.jpeg',
+      })
+      .expect(400);
+  });
+
+  it('3 - Deve autenticar o Usuario (Login)', async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/usuarios/logar')
+      .send({
+        usuario: 'murilo@email.com',
+        senha: 'murilo123',
+      })
+      .expect(200);
+
+    token = (resposta.body as { token: string }).token;
+  });
+
+  it('4 - Deve listar todos os usuários', async () => {
+    return request(app.getHttpServer())
+      .get('/usuarios/all')
+      .set('Authorization', token)
+      .send({})
+      .expect(200);
+  });
+
+  it('5 - Deve atualizar um Usuario', async () => {
+    return request(app.getHttpServer())
+      .put('/usuarios/atualizar')
+      .set('Authorization', token)
+      .send({
+        id: usuarioId,
+        nome: 'Murilo atualizado',
+        usuario: 'atualizado@email.com',
+        senha: 'murilo456',
+        foto: 'https://i.imgur.com/zEM4Z3S.jpeg',
+      })
+      .expect(200)
+      .then((resposta) => {
+        const body = resposta.body as { nome: string };
+
+        expect(body.nome).toEqual('Murilo atualizado');
+      });
   });
 });
