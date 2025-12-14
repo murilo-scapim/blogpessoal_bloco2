d0280c66347a3c141f4e6526767a61ca840f8a0c
murilo-scapim <mhscapim@gmail.com>
2025-12-14 16:26:28 -0300

    docs(swagger): adiciona decorators na entidade e na controller de Usuario

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,29 +1,35 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Postagem } from '../../postagem/entities/postagem.entity';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_usuarios' })
 export class Usuario {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
   nome: string;
 
+  @ApiProperty({ example: 'email@email.com.br' })
   @IsEmail()
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
   usuario: string;
 
+  @ApiProperty()
   @MinLength(8)
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
   senha: string;
 
+  @ApiProperty()
   @Column({ length: 5000 })
   foto: string;
 
+  @ApiProperty()
   @OneToMany(() => Postagem, (postagem) => postagem.usuario)
   postagem: Postagem[];
 }
